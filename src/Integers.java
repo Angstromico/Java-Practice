e90f9e7cc8ca4fc5dc0c00d27afe6be51ee0ea4c
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-12-13 11:29:00 -0400

    feat: integers

---
+++ src/Integers.java
@@ -0,0 +1,11 @@
+public class Integers {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 88;
+        double c = 13.14;
+
+        double total = a + b +c;
+
+        System.out.println(total);
+    }
+}
