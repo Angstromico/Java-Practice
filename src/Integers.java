1c5674d1f01a97c2d02b12f6406bb53c2f19172c
Manuel esteban Morales Zuarez <manuesteban1990@gmail.com>
2025-12-13 22:31:46 -0400

    feat: More practice with Integers

--- src/Integers.java
+++ src/Integers.java
@@ -11,5 +11,26 @@ public class Integers {
         double theLastOne = 1000 - total;
 
         System.out.println(theLastOne);
+
+        int minValue = Integer.MIN_VALUE;
+
+        System.out.println(minValue);
+
+        int maxValue = Integer.MAX_VALUE;
+
+        System.out.println(maxValue);
+
+        String minMessage = "The minimum integer value is = " + minValue;
+        String maxMessage = "The maximum integer value is = " + maxValue;
+        System.out.println(minMessage);
+        System.out.println(maxMessage);
+
+        String bustedMinValue = "Busted min value = " + (minValue - 1);;
+        String bustedMaxValue = "Busted max value = " + (maxValue + 1);;
+        System.out.println(bustedMinValue);
+        System.out.println(bustedMaxValue);
+
+        int underscoreInt = 2_147_483_647;
+        System.out.println("Underscore int = " + underscoreInt);
     }
 }
