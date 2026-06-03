e17c8d13c08d4f9c660fa23601d001d822df82c0
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-03 16:41:25 -0400

    feat: basic if

---
+++ src/If.java
@@ -0,0 +1,11 @@
+public class If {
+    public static void main(String args[]) {
+        var condition = true;
+
+        if(condition) {
+            System.out.println("True condition");
+        } else {
+            System.out.println("False condition");
+        }
+    }
+}
